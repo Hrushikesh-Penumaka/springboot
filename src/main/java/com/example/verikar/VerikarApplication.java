d5317517c3d2cc6e2a9f2f485b65e49e17c24f63
Hrushikesh-Penumaka <penumaka.hrushikesh.cse20@itbhu.ac.in>
2022-10-19 00:30:29 +0530

    created project

---
+++ src/main/java/com/example/verikar/VerikarApplication.java
@@ -0,0 +1,13 @@
+package com.example.verikar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerikarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerikarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/verikar/VerikarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.verikar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VerikarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
